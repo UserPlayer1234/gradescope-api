80ebd1a2592ca523204ecce6f500521982df84f7
UserPlayer1234 <theuserplayer1234@berkeley.edu>
2026-05-29 16:21:05 -0700

    fix: updated tests

--- tests/test_graders.py
+++ tests/test_graders.py
@@ -3,29 +3,27 @@ import pytest
 from gradescopeapi.classes.account import Account
 
 
-@pytest.mark.skip(reason="Not testing graders")
 def test_get_assignment_graders_non_empty(create_session):
     """Test getting graders for a question that has been graded."""
     # create test session
     test_session = create_session("instructor")
     account = Account(test_session)
 
-    course_id = "753413"
-    question_id = "49653137"
+    course_id = "1302606"
+    question_id = "70001340"
 
     graders = account.get_assignment_graders(course_id, question_id)
     assert len(graders) > 0, "Should have at least 1 grader"
 
 
-@pytest.mark.skip(reason="Not testing graders")
 def test_get_assignment_graders_empty(create_session):
     """Test getting graders for a question that has not been graded."""
     # create test session
     test_session = create_session("instructor")
     account = Account(test_session)
 
-    course_id = "753413"
-    question_id = "49653136"
+    course_id = "1302606"
+    question_id = "70505553"
 
     graders = account.get_assignment_graders(course_id, question_id)
     assert len(graders) == 0, "Should not have any graders"
